926ddae0e1231689bbe8a176c3dd38344cc1f2bc
okradonkey <okradonkey@disroot.org>
2020-04-22 17:25:59 -0500

    Fix stupid github mistakes
    Sometimes I hate github

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# FreeTheNipple
- RimWorld Mod
+# Free The Nipple
+A RimWorld mod:
+
+Uncovered torsos no longer cause women to feel naked and embarrassed.
+(Uncovered legs still cause both men and women to feel naked.)
+
+### Compatibility
+- Compatible with RimWorld **v1.1** and the **Royalty** DLC.
+- Should be compatible with any mods.
+
+### Notes
+- Should be safe to add or remove at any time from your save.
+- Can be placed anywhere in the mod list after Harmony.
+- Doesn't add or remove anything - it just alters the PsychologicallyNude method to evaluate females the same as males.
+- [Release notes](https://github.com/okradonkey/FreeTheNipple/releases/)
+
+### Inspired by
+u/warm-rimmer
+
+### How to use
+No special instructions.
+
+# Installation
+Add _Free The Nipple_ anywhere in your load order.
+- Subscribe at the [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2066062228)
+
+ _or_
+
+- Download the latest zip file from [Latest Release](https://github.com/okradonkey/FreeTheNipple/releases)
+- Unzip the contents and place them in your RimWorld/Mods folder.
+- Activate the mod in the mod menu in the game.
--- Source/FreeTheNipple.cs
+++ Source/FreeTheNipple.cs
@@ -10,40 +10,67 @@ using OpCodes = System.Reflection.Emit.OpCodes;
 namespace FreeTheNipple
 {
 
-    //Pawn_ApparelTracker.PsychologicallyNude
+    // HugsLib log observations, just for my own reference:
+    // This is just a map of which code strings appear where in a HugsLib log:
+    // 
+    // Loaded mods:
+    // Free the Nipple(OkraDonkey.FreeTheNipple): FreeTheNipple(av:1.0.0.2,fv:1.0.0.3)
+    // <Title from About.xml>(<packageId from About.xml>): <AssemblyName from project properties>(av:<AssemblyVersion from project properties>,fv:<AssemblyFileVersion from project properties>)
+    // 
+    // Active Harmony patches:
+    // Pawn_ApparelTracker.get_PsychologicallyNude: TRANS: FreeTheNipple.Remove_Shirt_Test.Transpiler
+    // <vanillaClass.vanillaMethod>: <PatchType>: <namespace>.<patchClassName>.<patchEnumerableName>
 
     [StaticConstructorOnStartup]
     public static class HarmonyPatches
     {
         static HarmonyPatches()
         {
-            var harmonyInstance = new Harmony(id: "RimWorld.OkraDonkey.FreeTheNipple.main");
+            // Harmony_id is only used by other patches that might want to load before/after this one
+            Harmony harmonyInstance = new Harmony(id: "RimWorld.OkraDonkey.FreeTheNipple.main");
             harmonyInstance.PatchAll();
         }
     }
 
+    // Targeting Pawn_ApparelTracker.PsychologicallyNude
+    // PsychologicallyNude is a property, so specify MethodType.Getter
+    // (It's actually get_PsychologicallyNude)
     [HarmonyPatch(typeof(Pawn_ApparelTracker), "PsychologicallyNude", MethodType.Getter)]
-    public static class Pawn_ApparelTracker_PsychologicallyNude_Transpiler
+    public static class Remove_Shirt_Test
     {
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
 
-            // In PsychologicallyNude IL code:
+            // Transpiler overview:
+            //   The vanilla code checks males for pants, and females for pants then shirts.
+            //   Instead of rewriting the entire section, we'll just remove the shirt-test for females.
+            //   There will still be separate tests for males and females, but they will be identical.
+            //
+            // IL Code Observations:
+            //   At the beginning of the PsychologicallyNude method, two local booleans are created: hasPants and hasShirt.
+            //   These are then assigned values by the HasBasicApparel(hasPants, hasShirt) method
+            //   In IL code, these variables are then recalled as follows:
             //   ldloc.0 --> hasPants
             //   ldloc.1 --> hasShirt
-            // Find the hasPants with a hasShirt two lines later.
-            // Remove the hasShirt and the previous line of code (which branches after hasPants).
-            // hasPants alone will then determine if the pawn is PsychologicallyNude.
+            //
+            // Strategy:
+            //   Find the hasPants with a hasShirt two IL lines later.
+            //   Remove the hasShirt and the previous line of code (which would branch if hasPants was false).
+            //   hasPants alone will then determine if the pawn is PsychologicallyNude.
+            //   (The short branch to which hasPants=false would have diverted will no longer have an entry point.)
 
+            // Set a marker so we can tell if we've found our target, and if so, where
             int pantsTestLine = -1;
 
+            // Load all the IL lines (from the vanilla method) and go through them one by one
             var codes = new List<CodeInstruction>(instructions);
             for (int i = 0; i < codes.Count; i++)
             {
-                if (codes[i].opcode == OpCodes.Ldloc_0) // hasPants
+                if (codes[i].opcode == OpCodes.Ldloc_0) // Does this line recall the local variable hasPants?
                 {
-                    if (codes[i + 2].opcode == OpCodes.Ldloc_1) // hasShirt
+                    if (codes[i + 2].opcode == OpCodes.Ldloc_1) // Does this later line recall the local variable hasShirt?
                     {
+                        // If yes and yes, we've identified our target
                         pantsTestLine = i;
                         break;
                     }
@@ -51,13 +78,17 @@ namespace FreeTheNipple
             }
             if (pantsTestLine > -1)
             {
+                // We found what we were looking for; let's remove those two IL lines
                 Log.Message("Free the Nipple: The nipple has been freed");
                 codes.RemoveRange(pantsTestLine + 1, 2);
             }
             else
             {
-                Log.Warning("Free The Nipple: Failed to remove female shirt test");
+                // Something went wrong and we didn't find the shirt-test
+                Log.Warning("Free The Nipple: Failed to remove female shirt-test");
             }
+            // We've either removed our two lines, or made no changes
+            // Now we teturn the IL codes to Harmony for reinsertion
             return codes.AsEnumerable();
         }
     }
